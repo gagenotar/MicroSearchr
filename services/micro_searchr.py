8733904e7a981f1c86f3115b6c0821b7ef56a2a1
Gage Notargiacomo <gagenotar@gmail.com>
2023-10-26 19:36:20 -0400

    Implemented the search method in the searchr service

--- services/micro_searchr.py
+++ services/micro_searchr.py
@@ -1,24 +1,44 @@
+import json
+
 from utils.openai_client import OpenAIClient, get_openai_client
-from schemas.micro_searchr import MicroSearchrResponse
+from utils.searchr_client import SearchrClient
+from schemas.searchr_response import MicroSearchrResponse
 
 
 class MicroSearchrService:
-    def __init__(self, openai_client: OpenAIClient):
+    def __init__(self, openai_client: OpenAIClient, searchr_client: SearchrClient):
         self.openai_client = openai_client
-        # TODO: add serper api client here
+        self.searchr_client = searchr_client
+        self._search_prompt = """
+        "Summarize the serper.dev search results array, based on the user's query, in 150 words or less.
+        The user's query was: {0}
+        """
 
-    @staticmethod
-    def search(query: str) -> MicroSearchrResponse:
+    # @staticmethod
+    def search(self, query: str) -> MicroSearchrResponse:
         # Search query using serper api
-
-        # extract context from serper result
+        responses = self.searchr_client.returnResponses(query)
 
         # send context + query + styling to OpenAIClient
+        system_prompt = self._search_prompt.format(query)
+
+        search_summary = self.openai_client.generate_text(
+            prompt=responses,
+            max_tokens=300,
+            system_prompt=system_prompt
+        )        
 
         # return response
-        return NotImplementedError
+        print(search_summary)
+        return search_summary
 
 
 def get_micro_searchr_service():
     openai_client: OpenAIClient = get_openai_client()
-    return MicroSearchrService(openai_client)
+    searchr_client: SearchrClient = SearchrClient()
+    return MicroSearchrService(openai_client, searchr_client)
+
+if __name__ == '__main__':
+    searchr_service = get_micro_searchr_service()
+    new_event = searchr_service.search("bannana hammock")
+    print(new_event)
